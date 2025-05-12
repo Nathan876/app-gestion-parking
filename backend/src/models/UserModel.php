24ed1a6cdb1aa3464712a10500372f431b611748
Nathan876 <sequins.trancheur0c@icloud.com>
2025-05-12 19:11:44 +0200

    Ajout du model User

---
+++ backend/src/models/UserModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\models;
+
+use PDO;
+use stdClass;
+
+class UserModel extends SqlConnect {
+    public function add(array $data) {
+        $query = "
+        INSERT INTO users (last_name, first_name, email, password, birth_date, role, phone_number, license_plate)
+        VALUES (:last_name, :first_name, :email, :password, :birth_date, :role, :phone_number, :license_plate)
+      ";
+
+        $req = $this->db->prepare($query);
+        $req->execute($data);
+    }
+
+    public function delete(int $id) {
+        $req = $this->db->prepare("DELETE FROM users WHERE id = :id");
+        $req->execute(["id" => $id]);
+    }
+
+    public function get(int $id) {
+        $req = $this->db->prepare("SELECT * FROM users WHERE id = :id");
+        $req->execute(["id" => $id]);
+
+        return $req->rowCount() > 0 ? $req->fetch(PDO::FETCH_ASSOC) : new stdClass();
+    }
+
+    public function getLast() {
+        $req = $this->db->prepare("SELECT * FROM users ORDER BY id DESC LIMIT 1");
+        $req->execute();
+
+        return $req->rowCount() > 0 ? $req->fetch(PDO::FETCH_ASSOC) : new stdClass();
+    }
+}
